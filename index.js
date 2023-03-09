5faf723dcd9af9b897ad443b4c218bf3821f9fb6
Elias <elias.groll97@gmail.com>
2023-03-09 23:55:30 +0100

    email api

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const fetch = require('cross-fetch');
 const express = require('express');
+const mail = require('@sendgrid/mail');
+const contacts = require('@sendgrid/client');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+// in latest body-parser use like below.
+app.use(bodyParser.urlencoded({extended: true}));
 const port = 3000;
-const THIRTY_MINUTES = 60*1000*30;
+const THIRTY_MINUTES = 60 * 1000 * 30;
 let placesData = {};
+const GPLACE_ID = 'ChIJszx8xfQMl0cRT2u02_0_K94';
+const GPLACES_KEY = 'AIzaSyCRPFAqXzH1w4oYxkN73urCJKgNWikOQ9c';
+const SENDGRID_API_KEY = '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';
+const SENDER_EMAIL = 'team@duy-restaurant.de';
+mail.setApiKey(SENDGRID_API_KEY);
+contacts.setApiKey(SENDGRID_API_KEY);
 const log = (s) => {
     console.log(new Date() + ': ' + s);
 }
 const getGooglePlaceData = async () => {
     const url = new URL('https://maps.googleapis.com/maps/api/place/details/json');
-    url.searchParams.append('place_id', 'ChIJszx8xfQMl0cRT2u02_0_K94')
-    url.searchParams.append('key', 'AIzaSyBL9pVe61LKssUfkAXnJjrOcdJXmyHNJIs')
+    url.searchParams.append('place_id', GPLACE_ID)
+    url.searchParams.append('key', GPLACES_KEY)
     url.searchParams.append('language', 'de_DE');
 
     return fetch(url)
@@ -23,6 +36,36 @@ app.use(express.static('public'));
 app.get('/api/places', (req, res) => {
     res.status(200).send(placesData);
 });
+
+app.get('/api/view', (req, res) => {
+
+    res.end(200);
+});
+
+app.post('/api/book', bodyParser.urlencoded({ extended: true }), async (req, res) => {
+    const {email, name, numPeople, Date} = req.body;
+    console.log(req);
+    await mail.send(newEmail(email, name, numPeople, Date))
+    await contacts.request({
+        url: '/v3/marketing/contacts',
+        method: 'PUT',
+        body: {'list_ids': ['All Contacts'], 'contacts': [{email}]},
+    });
+    res.end(200);
+})
+
+const newEmail = (email, name, numPeople, Date) => {
+    const mail = {
+        to: [email],
+        from: SENDER_EMAIL,
+        cc: ['team@restaurant-duy.de'],
+        subject: `Deine Reservierung bei Duy für ${numPeople} Personen am ${Date}.`,
+        text: `Vielen Dank für deine Reservierung bei Duy's Restaurant für ${numPeople} Personen! 
+                Wir schicken dir bald eine Bestätigung.`
+    }
+    console.log(mail);
+    return mail;
+}
 
 (async () => {
         placesData = await getGooglePlaceData();
--- public/js/main.js
+++ public/js/main.js
@@ -763,3 +763,135 @@ new VenoBox({
     share: true,
     spinner: 'rotating-plane'
 });
+
+const cookieconsent = initCookieConsent();
+cookieconsent.run({
+    current_lang: 'de',
+    autoclear_cookies: true,                   // default: false
+    page_scripts: true,                        // default: false
+
+    // mode: 'opt-in'                          // default: 'opt-in'; value: 'opt-in' or 'opt-out'
+    // delay: 0,                               // default: 0
+    // auto_language: null                     // default: null; could also be 'browser' or 'document'
+    // autorun: true,                          // default: true
+    // force_consent: false,                   // default: false
+    // hide_from_bots: false,                  // default: false
+    // remove_cookie_tables: false             // default: false
+    // cookie_name: 'cc_cookie',               // default: 'cc_cookie'
+    // cookie_expiration: 182,                 // default: 182 (days)
+    // cookie_necessary_only_expiration: 182   // default: disabled
+    // cookie_domain: location.hostname,       // default: current domain
+    // cookie_path: '/',                       // default: root
+    // cookie_same_site: 'Lax',                // default: 'Lax'
+    // use_rfc_cookie: false,                  // default: false
+    // revision: 0,                            // default: 0
+
+    onFirstAction: function (user_preferences, cookie) {
+        // callback triggered only once
+    },
+
+    onAccept: function (cookie) {
+        // ...
+    },
+
+    onChange: function (cookie, changed_preferences) {
+        // ...
+    },
+
+    languages: {
+        'en': {
+            consent_modal: {
+                title: 'Wir verwenden cookies!',
+                description: 'Hallo, diese Website verwendet essentielle Cookies, um ihren ordnungsgemäßen Betrieb zu gewährleisten, und Tracking-Cookies, um zu verstehen, wie Sie mit ihr interagieren. Letztere werden nur nach Zustimmung gesetzt werden. <button type="button" data-cc="c-settings" class="cc-link">Lass mich auswählen</button>',
+                primary_btn: {
+                    text: 'Alle akzeptieren',
+                    role: 'accept_all'              // 'accept_selected' or 'accept_all'
+                },
+                secondary_btn: {
+                    text: 'Alle verweigern',
+                    role: 'accept_necessary'        // 'settings' or 'accept_necessary'
+                }
+            },
+            settings_modal: {
+                title: 'Cookie preferences',
+                save_settings_btn: 'Einstellungen speichern',
+                accept_all_btn: 'Alle akzeptieren',
+                reject_all_btn: 'Alle verweigern',
+                close_btn_label: 'Schließen',
+                cookie_table_headers: [
+                    {col1: 'Name'},
+                    {col2: 'Domain'},
+                    {col3: 'Ablaufdatum'},
+                    {col4: 'Beschreibung'}
+                ],
+                blocks: [
+                    {
+                        title: 'Cookie Verwendung 📢',
+                        description: 'Ich verwende Cookies, um die grundlegenden Funktionen der Website zu gewährleisten und um Ihr Online-Erlebnis zu verbessern. Sie können für jede Kategorie wählen, ob Sie sich ein- oder austragen möchten. Für weitere Einzelheiten zu Cookies und anderen sensiblen Daten lesen Sie bitte die vollständige <a href="dsgvo.html" class="cc-link">Datenschutzerklärung</a>.'
+                    }, {
+                        title: 'Notwendige cookies',
+                        description: 'Diese Cookies sind für das ordnungsgemäße Funktionieren meiner Website unerlässlich. Ohne diese Cookies würde die Website nicht richtig funktionieren.',
+                        toggle: {
+                            value: 'necessary',
+                            enabled: true,
+                            readonly: true          // cookie categories with readonly=true are all treated as "necessary cookies"
+                        }
+                    }, {
+                        title: 'Werbe und Analytics cookies',
+                        description: 'Diese Cookies ermöglichen es der Website, sich an die von Ihnen in der Vergangenheit getroffenen Auswahlen zu erinnern',
+                        toggle: {
+                            value: 'analytics',     // your cookie category
+                            enabled: false,
+                            readonly: false
+                        },
+                        cookie_table: [             // list of all expected cookies
+                            {
+                                col1: '^_ga',       // match all cookies starting with "_ga"
+                                col2: 'google.com',
+                                col3: '2 Jahre',
+                                col4: 'Dieser Cookie enthält Informationen wie die Website, die der Benutzer besucht hat, die Dauer des Besuchs und die Interaktionen des Benutzers auf der Website. Diese Informationen werden an Google gesendet, um zu helfen, die Nutzung der Website zu verstehen und zu analysieren.',
+                                is_regex: true
+                            },
+                            {
+                                col1: '_gid',
+                                col2: 'google.com',
+                                col3: '1 Tag',
+                                col4: 'Der Google Analytics-Cookie "__gid" speichert eine eindeutige ID, die verwendet wird, um statistische Daten über die Nutzung der Website durch einen einzelnen Benutzer zu generieren.',
+                            },
+                            {
+                                col1: 'fbp',
+                                col2: 'facebook.com',
+                                col3: '1 Tag',
+                                col4: 'enthält eine eindeutige ID, die verwendet wird, um den Benutzer beim Besuch der Website zu identifizieren und um zu verstehen, welche Produkte und Inhalte für den Benutzer von Interesse sind.',
+                            },
+                            {
+                                col1: 'tr',
+                                col2: 'facebook.com',
+                                col3: '1 Tag',
+                                col4: 'Enthält Informationen darüber, welche Produkte der Benutzer auf der Website angesehen hat, um personalisierte Werbung auf anderen Websites anzuzeigen',
+                            },
+                            {
+                                col1: 'act',
+                                col2: 'facebook.com',
+                                col3: '1 Tag',
+                                col4: 'Enthält Informationen darüber, welche Aktionen der Benutzer auf der Website ausgeführt hat, wie z.B. das Absenden eines Formulars oder das Kaufen eines Produkts.',
+                            },
+                            {
+                                col1: 'posthog_sessionid',
+                                col2: 'posthog.com',
+                                col3: '1 Tag',
+                                col4: 'Enthält eine eindeutige ID, die verwendet wird, um die Interaktionen des Benutzers mit der Website zu identifizieren und zu verfolgen.',
+                            },
+                            {
+                                col1: 'posthog_visit',
+                                col2: 'posthog.com',
+                                col3: '1 Tag',
+                                col4: 'Enthält Informationen darüber, wann der Benutzer die Website zuletzt besucht hat und wie lange der Besuch dauerte.',
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+    }
+});
